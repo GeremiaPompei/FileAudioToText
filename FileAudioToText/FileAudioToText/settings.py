b5f0ddc1bdd48b625664999afe01900e54acd965
Geremia <geremia.pompei@studenti.unicam.it>
2020-12-23 09:21:22 +0100

    Add django dependency

--- FileAudioToText/FileAudioToText/settings.py
+++ FileAudioToText/FileAudioToText/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'FileAudioToText.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['FileAudioToText/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
