ac34f79738be03ed04426558c4d89f4013341496
Geremia <geremia.pompei@studenti.unicam.it>
2020-12-24 03:04:40 +0100

    Aggiunta conversione video in audio

--- FileAudioToText/Converter/views.py
+++ FileAudioToText/Converter/views.py
@@ -3,16 +3,22 @@ from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, Http404
 from django.utils.encoding import filepath_to_uri
 import speech_recognition as sr
+import moviepy.editor as mp
+
 
 # Create your views here.
 def upload(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
-        pathAudio = 'media/'+uploaded_file.name
-        fs.save(pathAudio, uploaded_file)
-        text = conversion(pathAudio)
-        fs.delete(pathAudio)
+        path = 'media/'+uploaded_file.name
+        fs.save(path, uploaded_file)
+        video = mp.VideoFileClip(path)
+        path = path+'.wav'
+        video.audio.write_audiofile(path)
+        fs.delete(path.replace('.wav',''))
+        text = conversion(path)
+        fs.delete(path)
         return render(request,"upload.html", {"codedtxt": "data:text/plain;charset=utf-8,"+filepath_to_uri(text), "download": "Download", "txt": text})
     return render(request, 'upload.html')
 
